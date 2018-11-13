3f1b5376f3e9f68836c0cc76f99e938d2acb0b50
Ted Masters <>
2018-11-13 15:14:47 +0000

    *** празно резюме ***

--- Chessboard.lua
+++ Chessboard.lua
@@ -2,9 +2,9 @@ local p = {}
 
 local function comment_empty( row, col, colchar )
 	if ((row + col) % 2 == 1) then
-		return 'бяло поле ' .. colchar[col] .. row .. ''
+		return 'бяло поле ' .. colchar[col] .. row
 	else
-		return 'черно поле ' .. colchar[col] .. row .. ''
+		return 'черно поле ' .. colchar[col] .. row
 	end
 end
 
@@ -113,30 +113,46 @@ local function image_square( img_empty, pc, row, col, size )
 		end
 	end
 
-	return '[[File:Chess ' .. caseName .. '45.svg|' .. size .. 'x' .. size .. 'px|alt=' .. alt .. '|' .. alt .. '|link=]]'
+	return '[[File:Chess ' .. caseName .. '45.svg|' .. size .. 'x' .. size .. 'px|alt=' .. alt .. '|link=]]'
 end
 	
-local function innerboard(args, size, rev)
+local function innerboard(board_name, board_width, board_height, args, size, rev)
+	local colchar = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
 	local root = mw.html.create('div')
+	local img_name = board_name or 'Chessboard480.svg'
 	root:addClass('chess-board')
 		:css('position', 'relative')
-		:wikitext(string.format( '[[File:Chessboard480.svg|%dx%dpx|link=]]', 8 * size, 8 * size ))
-	
-	for trow = 1,8 do
-		local row = rev and trow or ( 9 - trow )
-		for tcol = 1,8 do
-			local col = rev and ( 9 - tcol ) or tcol
-			local piece = args[8 * ( 8 - row ) + col + 2] or ''
-			if piece:match( '%w%w' ) then
-				local img = image_square(piece:match('%w%w'), row, col, size )
+		:wikitext(string.format( '[[File:%s|%dx%dpx|link=]]', img_name, board_width * size, board_height * size ))
+
+	local widthValue = size .. 'px'
+	local heightValue = size .. 'px'
+	for trow = 1,board_height do
+		local row = rev and trow or ( board_height + 1 - trow )
+		local topValue = tostring(( trow - 1 ) * size) .. 'px'
+		for tcol = 1,board_width do
+			local leftValue = tostring(( tcol - 1 ) * size) .. 'px'
+			local col = rev and ( board_width + 1 - tcol ) or tcol
+			local piece = args[board_width * ( board_height - row ) + col + 2] or ''
+			local pc = piece:match( '%w%w' ) or '  '
+			local img = image_square(board_name == nil, pc, row, col, size)
+			if img then
 				root:tag('div')
 					:css('position', 'absolute')
 					:css('z-index', '3')
-					:css('top', tostring(( trow - 1 ) * size) .. 'px')
-					:css('left', tostring(( tcol - 1 ) * size) .. 'px')
-					:css('width', size .. 'px')
-					:css('height', size .. 'px')
+					:css('top', topValue)
+					:css('left', leftValue)
+					:css('width', widthValue)
+					:css('height', heightValue)
 					:wikitext(img)
+			else
+				root:tag('div')
+					:attr('title', comment_empty(row, col, colchar))
+					:css('position', 'absolute')
+					:css('z-index', '3')
+					:css('top', topValue)
+					:css('left', leftValue)
+					:css('width', widthValue)
+					:css('height', heightValue)
 			end
 		end
 	end
@@ -144,22 +160,22 @@ local function innerboard(args, size, rev)
 	return tostring(root)
 end
 
-function chessboard(args, size, rev, letters, numbers, header, footer, falign, align, clear)
-	function letters_row( rev, num_lt, num_rt )
-		local letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}
+function chessboard(board_name, board_width, board_height, args, size, rev, letters, numbers, header, footer, footer_align, align, clear)
+	function letters_row( rev, num_lt, num_rt, width )
+		local letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
 		local root = mw.html.create('')
 		if num_lt then
 			root:tag('td')
 				:css('vertical-align', 'inherit')
 				:css('padding', '0')
 		end
-		for k = 1,8 do
+		for k = 1,board_width do
 			root:tag('td')
 				:css('padding', '0')
 				:css('vartical-align', 'inherit')
 				:css('height', '18px')
 				:css('width', size .. 'px')
-				:wikitext(rev and letters[9-k] or letters[k])
+				:wikitext(rev and letters[board_width+1-k] or letters[k])
 		end
 		if num_rt then
 			root:tag('td')
@@ -168,12 +184,12 @@ function chessboard(args, size, rev, letters, numbers, header, footer, falign, a
 		end
 		return tostring(root)
 	end
-	
+
 	local letters_tp = letters:match( 'both' ) or letters:match( 'top' )
 	local letters_bt = letters:match( 'both' ) or letters:match( 'bottom' )
 	local numbers_lt = numbers:match( 'both' ) or numbers:match( 'left' )
 	local numbers_rt = numbers:match( 'both' ) or numbers:match( 'right' )
-	local width = 8 * size + 2
+	local width = board_width * size + 2
 	if ( numbers_lt ) then width = width + 18 end
 	if ( numbers_rt ) then width = width + 18 end
 
@@ -182,10 +198,7 @@ function chessboard(args, size, rev, letters, numbers, header, footer, falign, a
 		:addClass(align)
 		:css('clear', clear)
 		:css('text-align', 'center')
-		:css('max-width', (width + 8) .. 'px')
-	if( header and header ~= '' ) then
-		root:tag('div'):css('line-height', '130%'):wikitext(header)
-	end
+		:wikitext(header)
 	local div = root:tag('div')
 		:addClass('thumbinner')
 		:css('width', width .. 'px')
@@ -209,26 +222,26 @@ function chessboard(args, size, rev, letters, numbers, header, footer, falign, a
 			:css('vertical-align', 'inherit')
 			:css('width', '18px')
 			:css('height', size .. 'px')
-			:wikitext(rev and 1 or 8) 
+			:wikitext(rev and 1 or board_height) 
 	end
 	local td = tablerow:tag('td')
-		:attr('colspan', 8)
-		:attr('rowspan', 8)
+		:attr('colspan', board_width)
+		:attr('rowspan', board_height)
 		:css('padding', '0')
 		:css('vertical-align', 'inherit')
-		:wikitext(innerboard(args, size, rev))
-	
+		:wikitext(innerboard(board_name, board_width, board_height, args, size, rev))
+
 	if ( numbers_rt ) then 
 		tablerow:tag('td')
 			:css('padding', '0')
 			:css('vertical-align', 'inherit')
 			:css('width', '18px')
 			:css('height', size .. 'px')
-			:wikitext(rev and 1 or 8) 
+			:wikitext(rev and 1 or board_height) 
 	end
 	if ( numbers_lt or numbers_rt ) then
-		for trow = 2, 8 do
-			local idx = rev and trow or ( 9 - trow )
+		for trow = 2, board_height do
+			local idx = rev and trow or ( board_height + 1 - trow )
 			tablerow = b:tag('tr')
 				:css('vertical-align', 'middle')
 			if ( numbers_lt ) then 
@@ -257,7 +270,7 @@ function chessboard(args, size, rev, letters, numbers, header, footer, falign, a
 		div:tag('div')
 			:addClass('thumbcaption')
 			:css('font-size', '90%')
-			:css('text-align', falign)
+			:css('text-align', footer_align)
 			:wikitext(footer)
 	end
 
@@ -290,9 +303,9 @@ function convertArgsToFen( args, offset )
 	function nullOrWhitespace( s ) return not s or s:match( '^%s*(.-)%s*$' ) == '' end
 	function piece( s ) 
 		return nullOrWhitespace( s ) and 1
-		or s:gsub( '%s*(%a)(%a)%s*', function( a, b ) return b == 'l' and a:upper() or a end )
+			or s:gsub( '%s*(%a)(%a)%s*', function( a, b ) return b == 'l' and a:upper() or a end )
 	end
-	
+    
 	local res = ''
 	offset = offset or 0
 	for row = 1, 8 do
@@ -311,42 +324,42 @@ function p.board(frame)
 	local reverse = ( args.reverse or pargs.reverse or '' ):lower() == "true"
 	local letters = ( args.letters or pargs.letters or 'both' ):lower() 
 	local numbers = ( args.numbers or pargs.numbers or 'both' ):lower() 
-	local header = args[2] or pargs[2] or ''
-	local footer = args[67] or pargs[67] or ''
-	local falign = args.falign or pargs.falign or 'left'
-	local align = ( args[1] or pargs[1] or 'tright' ):lower()
+	local header = args.header or pargs.header or args[2] or pargs[2] or ''
+	local board_width = args.width or pargs.width or 8
+	local board_height = args.height or pargs.height or 8
+	local footer = args.footer or pargs.footer or args[board_width * board_height + 3] or pargs[board_width * board_height + 3] or ''
+	local footer_align = args.footer_align or pargs.footer_align or 'left'
+	local align = (args.align or pargs.align or args[1] or pargs[1] or 'tright' ):lower()
 	local clear = args.clear or pargs.clear or ( align:match('tright') and 'right' ) or 'none'
 	local fen = args.fen or pargs.fen
-		
+	local board_name = nil
+	if (board_width == 8) and (board_height == 8) then
+		board_name = 'Chessboard480.svg'
+	end
+
 	size = mw.ustring.match( size, '[%d]+' ) or '26' -- remove px from size
 	if (fen) then
-		align = args.align or pargs.align or 'tright'
-		clear = args.clear or pargs.clear or ( align:match('tright') and 'right' ) or 'none'
-		header = args.header or pargs.header or ''
-		footer = args.footer or pargs.footer or ''
-		falign = args.falign or pargs.falign or 'left'
-		return chessboard( convertFenToArgs( fen ), size, reverse, letters, numbers, header, footer, falign, align, clear )
+		return chessboard(board_name, board_width, board_height, convertFenToArgs(fen), size, reverse, letters, numbers, header, footer, footer_align, align, clear)
 	end
 	if args[3] then
-		return chessboard(args, size, reverse, letters, numbers, header, footer, falign, align, clear)
+		return chessboard(board_name, board_width, board_height, args, size, reverse, letters, numbers, header, footer, footer_align, align, clear)
 	else
-		return chessboard(pargs, size, reverse, letters, numbers, header, footer, falign, align, clear)
+		return chessboard(board_name, board_width, board_height, pargs, size, reverse, letters, numbers, header, footer, footer_align, align, clear)
 	end
 end
 
 function p.fen2ascii(frame)
 	-- {{#invoke:Chessboard|fen2ascii|fen=...}}
 	local b = convertFenToArgs( frame.args.fen )
-	local res = '|=\n'
+	local res = ''
 	local offset = 2
 	for row = 1,8 do
 		local n = (9 - row)
-		res = res .. n .. ' |' .. 
-			table.concat(b, '|', 8*(row-1) + 1 + offset, 8*(row-1) + 8 + offset) .. '|=\n'
+		res = res .. ' |' .. 
+		table.concat(b, '|', 8*(row-1) + 1 + offset, 8*(row-1) + 8 + offset) .. '\n'
 	end
 	res = mw.ustring.gsub( res,'\| \|', '|  |' )
 	res = mw.ustring.gsub( res,'\| \|', '|  |' )
-	res = res .. '   a  b  c  d  e  f  g  h'
 	return res
 end
 
