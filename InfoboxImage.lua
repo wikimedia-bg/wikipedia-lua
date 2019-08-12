662ecd17a9318ba276a681754915ba502f041e51
StanProg <>
2019-08-12 17:08:39 +0000

    Синхронизиране с английския шаблон: 839038061

--- InfoboxImage.lua
+++ InfoboxImage.lua
@@ -1,5 +1,6 @@
 -- Inputs:
 --    image - Can either be a bare filename (with or without the File:/Image: prefix) or a fully formatted image link
+--    page - page to display for multipage images (DjVu)
 --    size - size to display the image
 --    maxsize - maximum size for image
 --    sizedefault - default size to display the image if size param is blank
@@ -9,6 +10,7 @@
 --    center - set to yes, if the image has to be centered
 --    upright - upright image param
 --    suppressplaceholder - if yes then checks to see if image is a placeholder and suppresses it
+--    link - page to visit when clicking on image
 -- Outputs:
 --    Formatted image.
 -- More details available at the "Module:InfoboxImage/doc" page
@@ -41,24 +43,75 @@ local placeholder_image = {
     "Replace this image male.svg",
     "Silver - Replace this image female.svg",
     "Silver - Replace this image male.svg",
+    "Replace this image.svg",
+	"Cricket no pic.png",
+	"CarersLogo.gif",
+	"Diagram Needed.svg",
+	"Example.jpg",
+	"Image placeholder.png",
+	"No male portrait.svg",
+	"Nocover-upload.png",
+	"NoDVDcover copy.png",
+	"Noribbon.svg",
+	"No portrait-BFD-test.svg",
+	"Placeholder barnstar ribbon.png",
+	"Project Trains no image.png",
+	"Image-request.png",
+	"Sin bandera.svg",
+	"Sin escudo.svg",
+	"Replace this image - temple.png",
+	"Replace this image butterfly.png",
+	"Replace this image.svg",
+	"Replace this image1.svg",
+	"Resolution angle.png",
+	"Image-No portrait-text-BFD-test.svg",
+	"Insert image here.svg",
+	"No image available.png",
+	"NO IMAGE YET square.png",
+	"NO IMAGE YET.png",
+	"No Photo Available.svg",
+	"No Screenshot.svg",
+	"No-image-available.jpg",
+	"Null.png",
+	"PictureNeeded.gif",
+	"Place holder.jpg",
+	"Unbenannt.JPG",
+	"UploadACopyrightFreeImage.svg",
+	"UploadAnImage.gif",
+	"UploadAnImage.svg",
+	"UploadAnImageShort.svg",
+	"CarersLogo.gif",
+	"Diagram Needed.svg",
+	"No male portrait.svg",
+	"NoDVDcover copy.png",
+	"Placeholder barnstar ribbon.png",
+	"Project Trains no image.png",
+	"Image-request.png",
+	"Noimage.gif",
 }
 
 function i.IsPlaceholder(image)
     -- change underscores to spaces
     image = mw.ustring.gsub(image, "_", " ");
+    assert(image ~= nil, 'mw.ustring.gsub(image, "_", " ") must not return nil')
     -- if image starts with [[ then remove that and anything after |
     if mw.ustring.sub(image,1,2) == "[[" then
         image = mw.ustring.sub(image,3);
         image = mw.ustring.gsub(image, "([^|]*)|.*", "%1");
+        assert(image ~= nil, 'mw.ustring.gsub(image, "([^|]*)|.*", "%1") must not return nil')
     end
     -- Trim spaces
     image = mw.ustring.gsub(image, '^[ ]*(.-)[ ]*$', '%1');
-    -- remove file: or image: prefix if exists
-    if mw.ustring.lower(mw.ustring.sub(image,1,5)) == "file:" then
-        image = mw.ustring.sub(image,6);
-    end
-    if mw.ustring.lower(mw.ustring.sub(image,1,6)) == "image:" then
-        image = mw.ustring.sub(image,7);
+    assert(image ~= nil, "mw.ustring.gsub(image, '^[ ]*(.-)[ ]*$', '%1') must not return nil")
+    -- remove prefix if exists
+    local allNames = mw.site.namespaces[6].aliases
+    allNames[#allNames + 1] = mw.site.namespaces[6].name
+    allNames[#allNames + 1] = mw.site.namespaces[6].canonicalName
+    for i, name in ipairs(allNames) do
+        if mw.ustring.lower(mw.ustring.sub(image, 1, mw.ustring.len(name) + 1)) == mw.ustring.lower(name .. ":") then
+            image = mw.ustring.sub(image, mw.ustring.len(name) + 2);
+            break
+        end
     end
     -- Trim spaces
     image = mw.ustring.gsub(image, '^[ ]*(.-)[ ]*$', '%1');
@@ -82,7 +135,7 @@ function i.InfoboxImage(frame)
     if image == "&nbsp;" then
         return image;
     end
-    if frame.args["suppressplaceholder"] == "yes" then
+    if frame.args["suppressplaceholder"] ~= "no" then
         if i.IsPlaceholder(image) == true then
             return "";
         end
@@ -111,6 +164,8 @@ function i.InfoboxImage(frame)
         -- search for thumbnail images and add to tracking cat if found
         if mw.title.getCurrentTitle().namespace == 0 and (mw.ustring.find(image, "|%s*thumb%s*[|%]]") or mw.ustring.find(image, "|%s*thumbnail%s*[|%]]")) then
             return image .. "[[Category:Pages using infoboxes with thumbnail images]]";
+        elseif mw.title.getCurrentTitle().namespace == 0 then
+            return image .. "[[Category:Pages using deprecated image syntax]]";
         else
             return image;
         end
@@ -121,23 +176,32 @@ function i.InfoboxImage(frame)
     elseif mw.ustring.sub(image,1,5) == mw.ustring.char(127).."UNIQ" then
         -- Found strip marker at begining, so pass don't process at all
         return image;
+    elseif mw.ustring.sub(image,4,9) == "`UNIQ-" then
+        -- Found strip marker at begining, so pass don't process at all
+        return image;
     else
         local result = "";
+        local page = frame.args["page"];
         local size = frame.args["size"];
         local maxsize = frame.args["maxsize"];
         local sizedefault = frame.args["sizedefault"];
         local alt = frame.args["alt"];
+        local link = frame.args["link"];
         local title = frame.args["title"];
         local border = frame.args["border"];
         local upright = frame.args["upright"] or "";
+        local thumbtime = frame.args["thumbtime"] or "";
         local center= frame.args["center"];
         
-        -- remove file: or image: prefix if exists
-        if mw.ustring.lower(mw.ustring.sub(image,1,5)) == "file:" then
-            image = mw.ustring.sub(image,6);
-        end
-        if mw.ustring.lower(mw.ustring.sub(image,1,6)) == "image:" then
-            image = mw.ustring.sub(image,7);
+        -- remove prefix if exists
+        local allNames = mw.site.namespaces[6].aliases
+        allNames[#allNames + 1] = mw.site.namespaces[6].name
+        allNames[#allNames + 1] = mw.site.namespaces[6].canonicalName
+        for i, name in ipairs(allNames) do
+            if mw.ustring.lower(mw.ustring.sub(image, 1, mw.ustring.len(name) + 1)) == mw.ustring.lower(name .. ":") then
+                image = mw.ustring.sub(image, mw.ustring.len(name) + 2);
+                break
+            end
         end
         
         if maxsize ~= "" and maxsize ~= nil then
@@ -148,7 +212,7 @@ function i.InfoboxImage(frame)
             -- check to see if size bigger than maxsize
             if size ~= "" and size ~= nil then
                 local sizenumber = tonumber(mw.ustring.match(size,"%d*")) or 0;
-                local maxsizenumber = tonumber(mw.ustring.match(maxsize,"%d*"));
+                local maxsizenumber = tonumber(mw.ustring.match(maxsize,"%d*")) or 0;
                 if sizenumber>maxsizenumber and maxsizenumber>0 then
                     size = maxsize;
                 end
@@ -158,8 +222,15 @@ function i.InfoboxImage(frame)
         if (tonumber(size) or 0) > 0 then
             size = size .. "px";
         end
+        -- add px to sizedefault if just a number
+        if (tonumber(sizedefault) or 0) > 0 then
+            sizedefault = sizedefault .. "px";
+        end
         
         result = "[[File:" .. image;
+        if page ~= "" and page ~= nil then
+            result = result .. "|page=" .. page;
+        end
         if size ~= "" and size ~= nil then
             result = result .. "|" .. size;
         elseif sizedefault ~= "" and sizedefault ~= nil then
@@ -173,11 +244,19 @@ function i.InfoboxImage(frame)
         if alt ~= "" and alt ~= nil then
             result = result .. "|alt=" .. alt;
         end
+        if link ~= "" and link ~= nil then
+            result = result .. "|link=" .. link;
+        end
         if border == "yes" then
             result = result .. "|border";
         end
-        if upright ~= "" then
+        if upright == "yes" then
+            result = result .. "|upright";
+        elseif upright ~= "" then
             result = result .. "|upright=" .. upright;
+        end
+        if thumbtime ~= "" then
+            result = result .. "|thumbtime=" .. thumbtime;
         end
         if title ~= "" and title ~= nil then
             result = result .. "|" .. title;
