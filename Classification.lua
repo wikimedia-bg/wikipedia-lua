5c7bd6600a28cfc71243dc2eee2d7ca72c330ebb
Termininja <>
2019-12-29 22:40:56 +0000

    *** празно резюме ***

--- Classification.lua
+++ Classification.lua
@@ -7,24 +7,25 @@ function getTaxon(id)
 	local latinName = wikidata.claim({ ['args'] = { ['id'] = id, [1] = 'P225' } })
 	local bgLabel = wikidata.getLabel({ ['args'] = { ['id'] = id, ['lang'] = 'bg' } })
 
-	local result =
+	local result = ''
+	if parentTaxon and parentTaxon ~= 'Липсва стойност' then
+		result = getTaxon('Q' .. parentTaxon)
+	end
+	
+	result = result ..
 		'<tr><td style="text-align: right; padding-right: 5px;">' .. rank .. ':</td>' ..
 		'<td ' .. (bgLabel and '' or 'colspan="2"') .. ' style="text-align: left; white-space:nowrap;">' .. latinName .. '</td>' ..
 		'<td style="text-align: left;">[[' .. bgLabel .. ']]</td></tr>'
 
-	if parentTaxon and parentTaxon ~= 'Липсва стойност' then
-		local parentTaxonId = 'Q' .. parentTaxon
-		result = getTaxon(parentTaxonId) .. result
-	end
-	
 	return result
 end
 
 function p.test(frame)
 	local id = frame.args[1]
-	local result = getTaxon(id)
-
-	return '<table style="width:260px" border="1"><tr><td><table style="width:260px">' .. result .. '</table></td></tr></table>'
+	if id then
+		return '<table style="width:260px" border="1"><tr><td><table style="width:260px">' .. getTaxon(id) .. '</table></td></tr></table>'
+	end
+	return nil
 end
 
 return p
