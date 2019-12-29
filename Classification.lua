43bf4f3bf617483df2c88e3ec831e0260b72bef3
Termininja <>
2019-12-29 19:37:02 +0000

    *** празно резюме ***

--- Classification.lua
+++ Classification.lua
@@ -1,10 +1,19 @@
 local p = {}
 local wikidata = require("Модул:Wikidata")
+local wd = require("Модул:Wd")
+
+function getTaxon(id)
+
+end
 
 function p.test(frame)
 	local property = frame.args[1]
 	
-	return wikidata.claim(frame);
+	frame.args[1] = "P171"
+	frame.args["id"] = "Q1263887"
+	frame.args["parameter"] = "numeric-id"
+	
+	return wikidata.claim(frame)
 end
 
 return p
