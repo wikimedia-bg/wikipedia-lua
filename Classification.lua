8947b300a81de396a919d6bde98af8180e2c162e
Termininja <>
2019-12-29 21:15:07 +0000

    *** празно резюме ***

--- Classification.lua
+++ Classification.lua
@@ -19,9 +19,9 @@ function getTaxon(id)
 	local bgLabel = wikidata.getLabel(frame)
 
 	local result =
-		'<td style="text-align: right; padding-right: 5px;">' .. rank .. ':</td>' ..
-		'<td ' .. (bgLabel and '' or 'colspan="2"') .. ' style="text-align: left; white-space:nowrap;">' .. latinName .. '</td>' ..
-		'<td style="text-align: left;">' .. bgLabel .. '</td>'
+		'<tr><td style="text-align: right; padding-right: 5px;">' .. rank .. ':</td><br>' ..
+		'<td ' .. (bgLabel and '' or 'colspan="2"') .. ' style="text-align: left; white-space:nowrap;">' .. latinName .. '</td><br>' ..
+		'<td style="text-align: left;">[[' .. bgLabel .. ']]</td></tr>'
 
 	if parentTaxon and parentTaxon ~= 'Липсва стойност' then
 		local parentTaxonId = 'Q' .. parentTaxon
@@ -35,7 +35,7 @@ function p.test(frame)
 	local id = frame.args[1]
 	local result = getTaxon(id)
 
-	return '<table style="width:100%"><tr>' .. result .. '</tr></table>'
+	return '<table style="width:260px" border="1"><tr><td><table style="width:260px">' .. result .. '</table></td></tr></table>'
 end
 
 return p
