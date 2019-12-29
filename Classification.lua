49a86662acc95f56139d8de32c34e3ac4c95e712
Termininja <>
2019-12-29 19:20:37 +0000

    *** празно резюме ***

--- Classification.lua
+++ Classification.lua
@@ -2,9 +2,7 @@ local p = {}
 local wikidata = require("Модул:Wikidata")
 
 function p.test(frame)
-	local property = frame.args[1]
-	
-	return wikidata.claim(property);
+	return frame.args[1];
 end
 
 return p
