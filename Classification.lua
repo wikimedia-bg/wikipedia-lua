bf87dc3868738a1d3fc14fcad04ec2f94f9d3b6c
Termininja <>
2019-12-30 19:39:15 +0000

    без външни модули

--- Classification.lua
+++ Classification.lua
@@ -1,10 +1,9 @@
 local p = {}
-local wikidata = require("Модул:Wikidata")
-local noValue = 'Липсва стойност'
+local noValue = 'без стойност'
 local notAllowedParentTaxonRanks = {'grandorder', 'magnorder', 'надимперия' , noValue}
 local bs = '\''
-local fossilTaxonItemId = 23038290
-local monotypicTaxonItemId = 310890
+local fossilTaxonName = 'изкопаем таксон'
+local monotypicTaxonName = 'монотипен таксон'
 local localRank
 
 function isAllowedRank(rank)
@@ -26,20 +25,72 @@ function toBold(str)
 	return str and bs .. toItalic(str) .. bs or ''
 end
 
-function getTaxonClassification(id)
-	local parentTaxon = wikidata.claim({ ['args'] = { ['id'] = id, 'P171', ['parameter'] = 'numeric-id' } })
-	local rank = wikidata.claim({ ['args'] = { ['id'] = id, 'P105' } })
-	local latinName = wikidata.claim({ ['args'] = { ['id'] = id, 'P225' } })
-	local bgLabel = wikidata.getLabel({ ['args'] = { ['id'] = id, ['lang'] = 'bg' } })
-	local instanceOf = wikidata.claim({ ['args'] = { ['id'] = id, 'P31', ['parameter'] = 'numeric-id' } })
+local function orderedpairs(array, order)
+	if not order then return pairs(array) end
+	local i = 0
+	return function()
+		i = i + 1
+		if order[i] then
+			return order[i], array[order[i]]
+		end
+	end
+end
 
-	local fossil = (instanceOf and instanceOf == fossilTaxonItemId) and '†' or ''
+function getClaim(id, property, index)
+	local entity = mw.wikibase.getEntityObject(id)
+	if not entity or not entity.claims then return end
+	
+	local claimsss = entity.claims[property]
+	local sortindices = {}
+	for idx in pairs(claimsss) do
+		sortindices[#sortindices + 1] = idx
+	end
+
+	local comparator = function(a, b)
+		local rankmap = { deprecated = 2, normal = 1, preferred = 0 }
+		local ranka = rankmap[claimsss[a].rank or "normal"] ..  string.format("%08d", a)
+		local rankb = rankmap[claimsss[b].rank or "normal"] ..  string.format("%08d", b)
+		return ranka < rankb
+	end
+	table.sort(sortindices, comparator)
+	
+	local claim = claimsss[sortindices[1]]
+	if claim then
+		local mainsnak = claim.mainsnak 
+		if mainsnak and mainsnak.datavalue then
+			local value = mainsnak.datavalue.value
+			if value then
+				return value
+			end
+		end
+	end
+	return
+end
+
+function getPropertyValue(id, p)
+	local entity = mw.wikibase.getEntity(id)
+	if entity then
+		local values = entity:formatPropertyValues(p)
+		if values then
+			return values.value
+		end
+	end
+	return
+end
+
+function getTaxonClassification(id)
+	local parent = getClaim(id, 'P171', 1)
+	local parentTaxon = parent and parent.id or nil
+	local rank = mw.ustring.gsub(getPropertyValue(id, 'P105'), '^([^,]+).*$', '%1')
+	local latinName = mw.wikibase.getEntity(id)['claims']['P225'][1].mainsnak.datavalue.value
+	local bgLabel = mw.wikibase.getLabelByLang(id, 'bg')
+	local instanceOf = getPropertyValue(id, 'P31')
+	local fossil = (instanceOf and string.match(instanceOf, fossilTaxonName)) and '†' or ''
 	local isLocalTaxon = localRank == rank
-	local result = parentTaxon and parentTaxon ~= noValue and getTaxonClassification('Q' .. parentTaxon) or ''
+	local result = parentTaxon and parentTaxon ~= noValue and getTaxonClassification(parentTaxon) or ''
 	if isAllowedRank(rank) then
 		result = result .. '<tr><td style="text-align:right; padding-right:5px">' .. rank .. ':</td><td style="text-align:left; white-space:nowrap"'
-		
-		local latinNameText = mw.ustring.gsub(latinName, '(.)%w+%s', "%1. ")
+		local latinNameText = mw.ustring.gsub(latinName, '(.)%w+%s', '%1. ')
 		if bgLabel and mw.ustring.match(bgLabel, '[А-я]') then
 			bgLabel = mw.ustring.upper(mw.ustring.sub(bgLabel, 0, 1)) .. mw.ustring.sub(bgLabel, 2)
 			local bgLabelText = isLocalTaxon and toBold(bgLabel) or toLink(bgLabel)
@@ -62,8 +113,8 @@ end
 function p.get(frame)
 	local itemId = frame.args[1]
 	if itemId then
-		localRank = wikidata.claim({ ['args'] = { ['id'] = itemId, 'P105' } })
-		
+		localRank = getPropertyValue(itemId, 'P105')
+
 		return '<table style="width:260px" border="1"><tr><td><table style="width:260px">' .. getTaxonClassification(itemId) .. '</table></td></tr></table>'
 	end
 	
