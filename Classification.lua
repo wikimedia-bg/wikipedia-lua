0d6baf9bf7e60b62ec4e0526352d0d4ad1c6d459
Termininja <>
2019-12-30 10:06:36 +0000

    *** празно резюме ***

--- Classification.lua
+++ Classification.lua
@@ -1,30 +1,37 @@
 local p = {}
 local wikidata = require("Модул:Wikidata")
+local noValue = 'Липсва стойност'
 
-function getTaxon(id)
-	local parentTaxon = wikidata.claim({ ['args'] = { ['id'] = id, [1] = 'P171', ['parameter'] = 'numeric-id' } })
-	local rank = wikidata.claim({ ['args'] = { ['id'] = id, [1] = 'P105' } })
-	local latinName = wikidata.claim({ ['args'] = { ['id'] = id, [1] = 'P225' } })
+function getTaxonClassification(id)
+	local parentTaxon = wikidata.claim({ ['args'] = { ['id'] = id, 'P171', ['parameter'] = 'numeric-id' } })
+	local rank = wikidata.claim({ ['args'] = { ['id'] = id, 'P105' } })
+	local latinName = wikidata.claim({ ['args'] = { ['id'] = id, 'P225' } })
 	local bgLabel = wikidata.getLabel({ ['args'] = { ['id'] = id, ['lang'] = 'bg' } })
-
+	
 	local result = ''
-	if parentTaxon and parentTaxon ~= 'Липсва стойност' then
-		result = getTaxon('Q' .. parentTaxon)
+	if parentTaxon and parentTaxon ~= noValue then
+		result = getTaxonClassification('Q' .. parentTaxon)
 	end
 	
-	result = result ..
-		'<tr><td style="text-align: right; padding-right: 5px;">' .. (rank == 'Липсва стойност' and '(без ранг)' or rank) .. ':</td>' ..
-		'<td ' .. (bgLabel and '' or 'colspan="2"') .. ' style="text-align: left; white-space:nowrap;">' .. latinName .. '</td>' ..
-		'<td style="text-align: left;">[[' .. bgLabel .. ']]</td></tr>'
-
+	rank = rank == noValue and '(без ранг)' or rank
+	result = result .. '<tr><td style="text-align:right; padding-right:5px">' .. rank .. ':</td><td style="text-align:left; white-space:nowrap"'
+	
+	if bgLabel and mw.ustring.match(bgLabel, '[А-я]') then
+		bgLabel = mw.ustring.upper(mw.ustring.sub(bgLabel, 0, 1)) .. mw.ustring.sub(bgLabel, 2)
+		result = result .. '>' .. latinName .. '</td><td style="text-align: left">[[' .. bgLabel .. ']]</td>'
+	else
+		result = result .. 'colspan="2">[[' .. latinName .. ']]</td>'
+	end
+	
 	return result
 end
 
 function p.test(frame)
-	local id = frame.args[1]
-	if id then
-		return '<table style="width:260px" border="1"><tr><td><table style="width:260px">' .. getTaxon(id) .. '</table></td></tr></table>'
+	local itemId = frame.args[1]
+	if itemId then
+		return '<table style="width:260px" border="1"><tr><td><table style="width:260px">' .. getTaxonClassification(itemId) .. '</table></td></tr></table>'
 	end
+	
 	return nil
 end
 
