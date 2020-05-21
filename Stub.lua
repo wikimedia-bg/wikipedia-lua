bf9992df556cf8e2aac0ba79ce4dd85eb13c8058
Termininja <>
2020-05-21 12:47:00 +0000

    *** празно резюме ***

--- Stub.lua
+++ Stub.lua
@@ -5,38 +5,45 @@ local THEMES = {
 	{ '...', '...' }
 }
 
-function p.get(frame)
-	local themes = frame.args
-	--for i=1, 10 do
-	--	themes[i]
-	--end
-	local theme = 'биология'
-	local pic = 'Butterfly template.png'
-		
-	local image = '[[File:' .. pic .. '|30x60п]]'
-	local text = '\'\'Тази статия, свързана с [[' .. theme .. ']] все още е [[Уикипедия:Мъниче|мъниче]].  Помогнете на Уикипедия, като я [{{fullurl:{{FULLPAGENAME}}|action=edit}} редактирате] и разширите.\'\''
-	local category = '[[Категория:Мъничета за ' .. theme .. ']]'
-	
+local function printStub(theme, image)
+	local imageLink = string.format('[[File:%s|30x60п]]', image)
+	local link = tostring(mw.uri.canonicalUrl(mw.title.getCurrentTitle().fullText, 'action=edit'))
+	local stubLink = '[[Уикипедия:Мъниче|мъниче]]'
+	local text = string.format("''Тази статия, свързана с [[%s]] все още е %s. Помогнете на Уикипедия, като я [%s редактирате] и разширите.''", theme, stubLink, link)
 	local stub = mw.html.create()
 		:tag('div')
-			:addClass('boilerplate metadata plainlinks noprint')
+			:addClass('plainlinks')
 			:css('margin-top', '1em')
 			:css('display', 'table')
 			:tag('div')
 				:css('float', 'left')
 				:css('width', '32px')
 				:css('overflow', 'hidden')
-				:wikitext(image)
+				:wikitext(imageLink)
 				:done()
 			:tag('div')
 				:css('margin-left', '37px')
 				:wikitext(text)
-				:done()
-			:done()
-		:wikitext(category)
-		:allDone()
+				:allDone()
+	
+	return tostring(stub)
+end
 
-	return tostring(stub)	
+function p.get(frame)
+	local themes = frame.args
+	--for i=1, 10 do
+	--	themes[i]
+	--end
+	local theme = 'биология'
+	local image = 'Butterfly template.png'
+	
+	local stub = printStub(theme, image)
+	
+	if mw.title.getCurrentTitle().namespace == 0 then
+		stub = stub .. '[[Категория:Мъничета за ' .. theme .. ']]'
+	end
+	
+	return stub
 end
 
 return p
