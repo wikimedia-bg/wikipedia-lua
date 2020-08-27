f96768b74fc5b62c52baaf04d7b766bb7575a75f
Nk <>
2020-08-27 16:32:18 +0000

    *** празно резюме ***

--- Stub.lua
+++ Stub.lua
@@ -156,7 +156,7 @@ local THEMES = {
 	{ 'Мавриций', 'Flag of Mauritius.svg' },
 	{ 'Мадагаскар', 'Flag of Madagascar.svg|border' },
 	{ 'Мадрид', 'Flag of the Community of Madrid.svg' },
-	{ 'Майот', 'Flag of Mayotte (local).svg' },
+	{ 'Майот', 'Flag of Mayotte (local).svg|border' },
 	{ 'Малави', 'Flag of Malawi.svg' },
 	{ 'Малайзия', 'Flag of Malaysia.svg|border' },
 	{ 'Малдивите|Малдиви', 'Flag of Maldives.svg|border' },
