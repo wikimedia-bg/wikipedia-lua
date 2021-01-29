c30f61579bea854ed1373636b822516c8449d85a
Ted Masters <>
2021-01-29 14:12:42 +0000

    прецизиране на изразите; използване на балансирани групи; подреждане по вероятност на срещане

--- Stub.lua
+++ Stub.lua
@@ -741,41 +741,67 @@ local function checkStubSize()
 	local content = mw.title.getCurrentTitle():getContent()
 	if content then
 		local size = content:len()
+		local temp
+		content = mw.ustring.lower(content)
+
+		-- без шаблони и уикитаблици
+		content = mw.ustring.gsub(content, '%b{}', function(cap)
+			if mw.ustring.match(cap, '^{[{|}]') and mw.ustring.match(cap, '[}|]}$') then
+				cap = ''
+			end
+			return cap
+		end)
 		
-		-- без имена на раздели
-		content = mw.ustring.gsub(content, '\n=+[^=]+=+\n', '')
+		-- без източници
+		content = mw.ustring.gsub(content, '<%f[%w]ref%f[%W][^>]*/>', '') -- премахване на самозатварящи се ref тагове
+		content = mw.ustring.gsub(content, '<%f[%w]ref%f[%W][^>]*>.-</%f[%w]ref%f[%W][^>]*>', '')
 		
-		-- без категории
-		content = mw.ustring.gsub(content, '%[%[[Кк]атегория:[^%[%]]+%]%]', '')
+		-- без галерии
+		content = mw.ustring.gsub(content, '<%f[%w]gallery%f[%W][^>]*>.-</%f[%w]gallery%f[%W][^>]*>', '')
+		
+		-- без файлове и категории; обикновен текст вместо препратки
+		content = mw.ustring.gsub(content, '%b[]', function(cap)
+			local prefix = mw.ustring.match(cap, '^%[%[%s*([a-zа-я]+):')
+			
+			if prefix and (prefix == 'файл' or prefix == 'картинка' or prefix == 'категория' or
+							prefix == 'file' or prefix == 'image' or prefix == 'category') then
+				return ''
+			end
+			
+			-- [B A] => A
+			cap = mw.ustring.gsub(cap, '%[https?://[^%s%[%]]+([^%[%]]*)%]', '%1')
+			cap = mw.ustring.gsub(cap, '%[//[^%s%[%]]+([^%[%]]*)%]', '%1')
+			
+			-- [[A]] => A
+			cap = mw.ustring.gsub(cap, '%[%[([^%|%[%]]+)%]%]', '%1')
+			
+			-- [[B|A]] => A
+			cap = mw.ustring.gsub(cap, '%[%[[^%|%[%]]+%|([^%[%]]*)%]%]', '%1')
+			
+			return cap
+		end)
+		
+		-- без съдържание в клетки на неуикифицирани таблици
+		repeat
+			temp = content
+			content = mw.ustring.gsub(content, '<%f[%w]t[dh]%f[%W][^>]*>.-(</?%f[%w]t[dhr]%f[%W][^>]*>)', '%1')
+			content = mw.ustring.gsub(content, '<%f[%w]t[dh]%f[%W][^>]*>.-(</?%f[%w]table%f[%W][^>]*>)', '%1')
+		until content == temp
+		
+		-- без тагове
+		content = mw.ustring.gsub(content, '</?%f[%w][a-z]+%f[%W][^>]*>', '')
 		
 		-- без коментари
 		content = mw.ustring.gsub(content, '<!%-%-.-%-%->', '')
 		
-		-- без източници
-		content = mw.ustring.gsub(content, '<ref%s*.->.-</ref>', '')
-		
-		-- без галерии
-		content = mw.ustring.gsub(content, '<gallery%s*.->.-</gallery>', '')
-		
-		-- без таблици
-		content = mw.ustring.gsub(content, '<table%s*.->.-</table>', '')
-		content = mw.ustring.gsub(content, '%{%|.-%|%}', '')
-		
-		-- [A] => A
-		content = mw.ustring.gsub(content, '%[%[([^%|%[%]]+)%]%]', '%1')
-
-		-- [Картинка:F|A] и [B|A] => A
-		repeat temp = content
-			content = mw.ustring.gsub(content, '%[%[[^%[%]]+%|([^%[%]]+)%]%]', '%1')
+		-- без раздели
+		repeat
+			temp = content
+			content = mw.ustring.gsub(content, '\n(=+)[^\n]+%1%s*\n', '\n')
 		until content == temp
-
-		-- без шаблони
-		repeat temp = content
-			content = mw.ustring.gsub(content, '%{%{[^%{%}]+%}%}', '')
-		until content == temp
-
+		
 		-- поне 4 букви в дума на кирилица
-		local _, words = mw.ustring.gsub(content, '[А-я][А-я][А-я][А-я]+', '')
+		local _, words = mw.ustring.gsub(content, '[а-я][а-я][а-я][а-я]+', '')
 		if words >= 500 then
 			-- мъниче с над 500 думи
 			CATEGORY = CATEGORY .. '[[Категория:Мъничета с над 500 думи]]'
