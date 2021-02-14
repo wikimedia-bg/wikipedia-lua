917bce81b6d29bce3de783aed1490695ddb65888
Luchesar ILIEV <luchesar.iliev@gmail.com>
2021-02-14 18:05:18 +0000

    *** празно резюме ***

--- Старобългарски.lua
+++ Старобългарски.lua
@@ -1,7 +1,7 @@
 local p = {}
 
-local pref = '[[File:Cyrillic '
-local suff1 = ' Lazov.svg|x'
+local pref = '[[File:Cyrillic' .. ' '
+local suff1 = ' ' .. 'Lazov.svg|x'
 local suff2 = 'px|sub]]'
 
 local lookup = {
@@ -21,17 +21,17 @@ function p.render(frame)
 	local ostr = ''
 	local fsiz = frame.args[2]
 	if ( fsiz == nil or fsiz == '' ) then fsiz = '20' end
-	for l in mw.ustring.gmatch( istr, '.' ) do
-		if l == ' ' then
-			ostr = ostr .. ' '
+	for l in mw.ustring.gmatch(istr, '.') do
+		if l == mw.ustring.lower(l) then
+			case = 'small' .. ' '
 		else
-			if l == mw.ustring.lower(l) then
-				case = 'small '
-			else
-				case = 'capital '
-				l = mw.ustring.lower(l)
-			end
+			case = 'capital' .. ' '
+			l = mw.ustring.lower(l)
+		end
+		if lookup[l] ~= nil then
 			ostr = ostr .. pref .. case .. lookup[l] .. suff1 .. fsiz .. suff2
+		else
+			ostr = ostr .. l
 		end
 	end
 	return ostr
