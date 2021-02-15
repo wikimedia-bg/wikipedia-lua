75926c4143aecfdbee89e3ca11133e755518efe3
Luchesar ILIEV <luchesar.iliev@gmail.com>
2021-02-15 10:48:30 +0000

    *** празно резюме ***

--- Старобългарски.lua
+++ Старобългарски.lua
@@ -1,10 +1,21 @@
 local p = {}
 
+-- For each letter we build a wiki markup to display the relevant SVG image.
+-- The SVG images have names with the following structure:
+-- File:Cyrillic [small|capital] <letter name> Lazov.svg
+-- Vertical size is set with "xNpx", where N is the default size (see below).
+-- Vertical alignment is set to "sub". The generated file link is disabled.
 local pref = '[[File:Cyrillic' .. ' '
 local suff1 = ' ' .. 'Lazov.svg|x'
 local suff2 = 'px|sub|link=]]'
+
+-- Default vertical size of the SVG letters, in pixels (xNpx).
+local vsize_def = '20'
+
+-- The spaces in the input text are replaced with this for better legibility.
 local spc = '<span style="display: inline-block; width: .5em;"> </span>'
 
+-- The substitute (lookup) table: ['<input letter>'] = '<letter name>'
 local lookup = {
 	['а'] = 'a',
 	['б'] = 'be',
@@ -67,8 +78,8 @@ local lookup = {
 function p.render(frame)
 	local istr = frame.args[1]
 	local ostr = ''
-	local fsiz = frame.args[2]
-	if ( fsiz == nil or fsiz == '' ) then fsiz = '20' end
+	local vsize = frame.args[2]
+	if ( vsize == nil or vsize == '' ) then vsize = vsize_def end
 	for l in mw.ustring.gmatch(istr, '.') do
 		if l == mw.ustring.lower(l) then
 			case = 'small' .. ' '
@@ -77,7 +88,7 @@ function p.render(frame)
 			l = mw.ustring.lower(l)
 		end
 		if lookup[l] ~= nil then
-			ostr = ostr .. pref .. case .. lookup[l] .. suff1 .. fsiz .. suff2
+			ostr = ostr .. pref .. case .. lookup[l] .. suff1 .. vsize .. suff2
 		elseif l == ' ' then
 			ostr = ostr .. spc
 		else
