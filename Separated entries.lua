483fcdd050473e741ae1b213667ac87c275b325b
Pl71 <>
2021-04-20 08:09:28 +0000

    Обновяване от английската версия

--- Separated entries.lua
+++ Separated entries.lua
@@ -1,7 +1,7 @@
 -- This module takes positional parameters as input and concatenates them with
 -- an optional separator. The final separator (the "conjunction") can be
 -- specified independently, enabling natural-language lists like
--- "foo, bar, baz and qux".
+-- "foo, bar, baz and qux". The starting parameter can also be specified.
 
 local compressSparseArray = require('Module:TableTools').compressSparseArray
 local p = {}
@@ -11,16 +11,22 @@ function p._main(args)
 		-- Decode (convert to Unicode) HTML escape sequences, such as "&#32;" for space.
 		and mw.text.decode(args.separator) or ''
 	local conjunction = args.conjunction and mw.text.decode(args.conjunction) or separator
+	-- Discard values before the starting parameter.
+	local start = tonumber(args.start)
+	if start then
+		for i = 1, start - 1 do args[i] = nil end
+	end
 	-- Discard named parameters.
 	local values = compressSparseArray(args)
 	return mw.text.listToText(values, separator, conjunction)
 end
 
-local function makeInvokeFunction(separator, conjunction)
+local function makeInvokeFunction(separator, conjunction, first)
 	return function (frame)
 		local args = require('Module:Arguments').getArgs(frame)
 		args.separator = separator or args.separator
 		args.conjunction = conjunction or args.conjunction
+		args.first = first or args.first
 		return p._main(args)
 	end
 end
