f367906cb0a1f882fb8278f7703b3803c0a57609
Ivanov id <>
2021-04-21 07:58:17 +0000

    *** празно резюме ***

--- Stub.lua
+++ Stub.lua
@@ -595,7 +595,7 @@ local THEMES = {
 
 		-- други
 		{ 'благородник|аристократ', 'Coat of arms of Brabant.svg', 'благородници' },
-		{ 'Българската академия на науките|БАН', 'Bulgarian Academy of Sciences fragment 2.jpg', 'Българска академия на науките|БАН' },
+		{ 'Българската академия на науките|БАН', 'Bulgarian Academy of Sciences fragment 2.jpg' },
 		{ 'български владетел', 'Crown of Bulgaria.svg', 'български владетели' },
 		{ 'индианец', 'NSRW Sitting Bull.jpg', 'индианци' },
 		{ 'монарх', 'Earlkrona, Nordisk familjebok.png', 'монарси' },
