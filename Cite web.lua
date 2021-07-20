1d83731986bb05e93b495857e0b071fe5ff15168
Stanqo <>
2021-07-20 08:49:12 +0000

    Нова страница: „local p = {} local CS1 = require('Module:Citation/CS1')  p[''] = function(frame) 	local newFrame = { 	    getParent = function(self) 	    	return frame 	    end, 	    getTitle = function(self) 	    	return 'Template:Cite web' 	    end, 	    args = {CitationClass='web'} 	} 	setmetatable(newFrame, { 		__index = function(t, k) 			if type(frame[k]) == 'function' then 				return function(...) 					return frame[k](frame, select(2, ...)) 				end 			else 				return...“

---
+++ Cite web.lua
@@ -0,0 +1,28 @@
+local p = {}
+local CS1 = require('Module:Citation/CS1')
+
+p[''] = function(frame)
+	local newFrame = {
+	    getParent = function(self)
+	    	return frame
+	    end,
+	    getTitle = function(self)
+	    	return 'Template:Cite web'
+	    end,
+	    args = {CitationClass='web'}
+	}
+	setmetatable(newFrame, {
+		__index = function(t, k)
+			if type(frame[k]) == 'function' then
+				return function(...)
+					return frame[k](frame, select(2, ...))
+				end
+			else
+				return frame[k]
+			end
+		end
+	})
+	return CS1.citation(newFrame)
+end
+
+return p
