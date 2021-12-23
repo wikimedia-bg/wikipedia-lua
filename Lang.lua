17e05c0a615d1b27d84dd27c58150334a105eed3
Ted Masters <>
2021-12-23 05:23:19 +0000

    *** празно резюме ***

--- Lang.lua
+++ Lang.lua
@@ -24,20 +24,30 @@ local function linkCheck(name)
 		else -- страницата не следва традиционен формат на името
 			qid = mw.wikibase.getEntityIdForTitle(name) -- друга проверка за съществуващо име
 			if qid then -- страницата съществува и е свързана с Уикиданни
-				local instance = mw.ustring.lower(mw.getCurrentFrame():callParserFunction{name = '#property', args = {'P31', from = qid }}) -- евтин начин за извикване на всички стойности на свойството; избягва се цикленето през отделните стойности и последващото допълнително анализиране
+				local instance = mw.wikibase.getAllStatements(qid, 'P31')
+				for i = 1, #instance do
+					local id = instance[i].mainsnak and instance[i].mainsnak.datavalue and instance[i].mainsnak.datavalue.value and instance[i].mainsnak.datavalue.value.id
+					if id then
+						local label = mw.ustring.lower(mw.wikibase.getLabel(id) or '')
+						if mw.ustring.match(label, '%f[%a]език%f[%A]') or mw.ustring.match(label, '%f[%a]language%f[%A]') then
+							return name -- страницата е екземпляр на език
+						end
+					end
+				end
+
 				local iso_1 = mw.wikibase.getAllStatements(qid, 'P218')
 				local iso_2 = mw.wikibase.getAllStatements(qid, 'P219')
 				local iso_3 = mw.wikibase.getAllStatements(qid, 'P220')
 				local ietf = mw.wikibase.getAllStatements(qid, 'P305')
-				if mw.ustring.match(instance, '%f[%a]език%f[%A]') or mw.ustring.match(instance, '%f[%a]language%f[%A]') or ((#iso_1 + #iso_2 + #iso_3 + #ietf) > 0) then
-					-- страницата е екземпляр на език или съдържа свойства, идентифициращи обекта като език
+				if (#iso_1 + #iso_2 + #iso_3 + #ietf) > 0 then
+					-- страницата съдържа свойства, идентифициращи обекта като език
 					return name
 				end
 			end
 		end
 	end
 
-	-- Страницата не свързана с Уикиданни или е свързана, но не е екземпляр на език и не съдържа свойства, идентифициращи обекта като език
+	-- страницата не свързана с Уикиданни или е свързана, но не е екземпляр на език и не съдържа свойства, идентифициращи обекта като език
 	return name .. (mw.ustring.match(name, '[жзсчцш]ки$') and ' език' or ' (език)')
 end
 
@@ -79,7 +89,7 @@ end
 function p.docTable(frame)
 	local ttype = mw.ustring.lower(frame.args[1] or '')
 
-	if ttype == '' then return '' end
+	if ttype ~= 'преведени' and ttype ~= 'непреведени' and ttype ~= 'липсващи' then return '' end
 
 	local div = mw.html.create('div')
 		:css('height', '400px')
@@ -96,58 +106,60 @@ function p.docTable(frame)
 	local translated = mw.html.create()
 	local not_translated = mw.html.create()
 	local missing = mw.html.create()
-	local result = ''
 	local background, link, name
 	local all_langs = {}
 
-	for k, v in pairs(mw.language.fetchLanguageNames('bg', 'all')) do
-		if type(k) == 'string' and type(v) == 'string' then
-			table.insert(all_langs, {k, v})
+	if ttype == 'преведени' or ttype == 'непреведени' then
+		for k, v in pairs(mw.language.fetchLanguageNames('bg', 'all')) do
+			if type(k) == 'string' and type(v) == 'string' then
+				table.insert(all_langs, {k, v})
+			end
+		end
+
+		table.sort(all_langs, function(a, b) return a[1] < b[1] end)
+
+		for i = 1, #all_langs do
+			name = data['renamed'][all_langs[i][1]] or all_langs[i][2]
+			if mw.ustring.match(mw.ustring.lower(name), '^[а-ъьюя%s%p]+$') then
+				if ttype == 'преведени' then
+					background = data['renamed'][all_langs[i][1]] and '#dff9f9' or nil
+					if data['renamed'][all_langs[i][1]] and data['renamed'][all_langs[i][1]] == all_langs[i][2] then
+						background = '#ffdbd4' 
+					end
+					link = data['link_exception'][all_langs[i][1]] or linkCheck(name)
+					if data['link_exception'][all_langs[i][1]] then
+						name = "''" .. name .. "''"
+					end
+					translated
+						:node(tableRow('td', nil, background, createLink(link, name), tempExample(all_langs[i][1])))
+						:newline()
+				end
+			else
+				if ttype == 'непреведени' then
+					not_translated
+						:node(tableRow('td', nil, nil, name, tempExample(all_langs[i][1])))
+						:newline()
+				end
+			end
 		end
 	end
 
-	table.sort(all_langs, function(a, b) return a[1] < b[1] end)
-
-	for i = 1, #all_langs do
-		name = data['renamed'][all_langs[i][1]] or all_langs[i][2]
-		if mw.ustring.match(mw.ustring.lower(name), '^[а-ъьюя%s%p]+$') then
-			background = data['renamed'][all_langs[i][1]] and '#dff9f9' or nil
-			if data['renamed'][all_langs[i][1]] and data['renamed'][all_langs[i][1]] == all_langs[i][2] then
-				background = '#ffdbd4' 
-			end
-			link = data['link_exception'][all_langs[i][1]] or linkCheck(name)
-			if data['link_exception'][all_langs[i][1]] then
-				name = "''" .. name .. "''"
-			end
-			translated
-				:node(tableRow('td', nil, background, createLink(link, name), tempExample(all_langs[i][1])))
-				:newline()
-		else
-			not_translated
-				:node(tableRow('td', nil, nil, name, tempExample(all_langs[i][1])))
-				:newline()
-		end
-	end
-
-	for k, v in pairs(data.missing) do
+	if ttype == 'липсващи' then
+		for k, v in pairs(data.missing) do
 		link = data['link_exception'][k] or linkCheck(v)
 		missing
 			:node(tableRow('td', nil, nil, createLink(link, v), tempExample(k)))
 			:newline()
+		end
 	end
 
 	if ttype == 'преведени' then
-		div:node(tab:node(translated))
-		result = tostring(div)
+		return tostring(div:node(tab:node(translated)))
 	elseif ttype == 'непреведени' then
-		div:node(tab:node(not_translated))
-		result = tostring(div)
+		return tostring(div:node(tab:node(not_translated)))
 	elseif ttype == 'липсващи' then
-		div:node(tab:node(missing))
-		result = tostring(div)
+		return tostring(div:node(tab:node(missing)))
 	end
-
-	return result
 end
 
 function p.cite(frame)		-- Опросена версия за {{cite-lang}}
