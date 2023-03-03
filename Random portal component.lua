3d87235bc687b36c3a152ec5442b95b63ba92e3a
Nauka <>
2023-03-03 13:15:23 +0000

    *** празно резюме ***

--- Random portal component.lua
+++ Random portal component.lua
@@ -103,7 +103,7 @@ local function getPages(args)
 		pages.random = pages.subpage .. '/' .. getRandomNumber(args.max)
 		tries = tries - 1
 	until tries < 1 or mw.title.new(pages.random).exists
-	pages.footer = 'Template:Box-footer'
+	pages.footer = 'Шаблон:Портал:Кутия-Край'
 	return pages
 end
 
