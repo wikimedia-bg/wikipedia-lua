e1962715486de9354fb24f092921de104785c6d2
Carbonaro. <>
2024-06-09 18:46:13 +0000

    *** празно резюме ***

--- Word count.lua
+++ Word count.lua
@@ -1,17 +1,18 @@
 local p = {}
 
 function p.count(frame)
-    local title = frame.args[1]
-    
-    local articleText = mw.title.new(title):getContent()
-    articleText = articleText:gsub("<ref.-</ref>","")
-    
-    local words = 0
-    for _ in articleText:gmatch("%S+") do
-        words = words + 1
-    end
-    
-    return words
+	local title = frame.args[1]
+	
+	local articleText = mw.title.new(title):getContent()
+	articleText = articleText:gsub("<ref name[^>]-/>","")
+	articleText = articleText:gsub("<ref.-</ref>","")
+	
+	local words = 0
+	for _ in articleText:gmatch("%S+") do
+		words = words + 1
+	end
+	
+	return words
 end
 
 return p
