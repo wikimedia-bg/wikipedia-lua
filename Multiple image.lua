d3c549571aef559253d5204f6314f5db51891bab
PaleoWolf <>
2026-08-17 16:43:56 +0000

    ъпдейт от енУ

--- Multiple image.lua
+++ Multiple image.lua
@@ -1,11 +1,16 @@
--- implements [[template:multiple image]]
+-- implements [[Template:Multiple image]]
 local p = {}
 
 local autoscaledimages
 local nonautoscaledimages
+local numberofimages = -1
 
 local function isnotempty(s)
 	return s and s:match( '^%s*(.-)%s*$' ) ~= ''
+end
+
+local function removepx(s)
+	return tostring(s or ''):match('^(.*)[Pp][Xx]%s*$') or s
 end
 
 local function getdimensions(s, w, h)
@@ -21,31 +26,39 @@ local function getdimensions(s, w, h)
 	return w, h
 end
 
-local function renderImageCell(image, width, height, link, alt, thumbtime, caption, textalign, istyle)
+local function renderImageCell(image, width, height, link, alt, thumbtime, caption, class, textalign, istyle, border, tcolor)
 	local root = mw.html.create('')
-
-	local altstr = '|alt=' .. (alt or '')
+	local altstr = ''
+	local classstr = class and ('|class=' .. class) or ''
 	local linkstr = link and ('|link=' .. link) or ''
 	local widthstr = '|' .. tostring(width) .. 'px'
 	local thumbtimestr = ''
-
+	if isnotempty( alt ) then
+		altstr = '|alt=' .. alt
+	end
+	if widthstr == '|-nanpx' then
+		widthstr = ''
+	end
 	if isnotempty( thumbtime ) then
 		thumbtimestr = '|thumbtime=' .. thumbtime
 	end
 
 	local imagediv = root:tag('div')
-	imagediv:addClass('thumbimage')
+	imagediv:addClass((border ~= 'infobox') and 'thumbimage' or nil)
 	imagediv:cssText(istyle)
-	if( height ) then
+	if height then
 		imagediv:css('height', tostring(height) .. 'px')
 		imagediv:css('overflow', 'hidden')
 	end
-	imagediv:wikitext('[[file:' .. image .. widthstr .. linkstr .. altstr .. thumbtimestr .. ']]')
+	imagediv:wikitext('[[File:' .. image .. classstr .. widthstr .. linkstr .. altstr .. thumbtimestr .. ']]')
 	if isnotempty(caption) then
 		local captiondiv = root:tag('div')
-		captiondiv:addClass('thumbcaption')
+		captiondiv:addClass((border ~= 'infobox') and 'thumbcaption' or nil)
 		if isnotempty(textalign) then
 			captiondiv:addClass('text-align-' .. textalign)
+		end
+		if tcolor ~= '' then
+			captiondiv:css('color', tcolor)
 		end
 		captiondiv:wikitext(caption)
 	end
@@ -88,23 +101,22 @@ end
 local function renderMultipleImages(frame)
 	local pargs = frame:getParent().args
 	local args = frame.args
-	local width = pargs['width'] or ''
+	local width = removepx(pargs['width'] or '')
 	local dir = pargs['direction'] or ''
 	local border = pargs['border'] or args['border'] or ''
 	local align = pargs['align'] or args['align'] or (border == 'infobox' and 'center' or '')
 	local capalign = pargs['caption_align'] or args['caption_align'] or ''
-	local totalwidth = pargs['total_width'] or args['total_width'] or ''
+	local totalwidth = removepx(pargs['total_width'] or args['total_width'] or '')
 	local imgstyle = pargs['image_style'] or args['image_style']
 	local header = pargs['header'] or pargs['title'] or ''
 	local footer = pargs['footer'] or ''
 	local imagegap = tonumber(pargs['image_gap'] or '1') or 1
-	local perrow = nil
 	local thumbclass = {
-		["left"] = 'tleft',
+		["left"] = 'floatleft',
 		["none"] = 'tnone',
 		["center"] = 'tnone',
 		["centre"] = 'tnone',
-		["right"] = 'tright'
+		["right"] = 'floatright'
 		}
 
 	-- find all the nonempty images
@@ -112,17 +124,19 @@ local function renderMultipleImages(frame)
 	local imagecount = 0
 	for k, v in pairs( pargs ) do
 		local i = tonumber(tostring(k):match( '^%s*image([%d]+)%s*$' ) or '0')
-		if( i > 0 and isnotempty(v) ) then
+		if i > 0 and isnotempty(v) then
 			table.insert( imagenumbers, i)
 			imagecount = imagecount + 1
 		end
 	end
-
+	-- save number of images for tracking category
+	numberofimages = imagecount
+	
 	-- sort the imagenumbers
 	table.sort(imagenumbers)
 
 	-- create an array with the number of images per row
-	perrow = getPerRow(dir == 'vertical' and '1' or pargs['perrow'], imagecount)
+	local perrow = getPerRow(dir == 'vertical' and '1' or pargs['perrow'], imagecount)
 
 	-- compute the number of rows
 	local rowcount = #perrow
@@ -137,9 +151,9 @@ local function renderMultipleImages(frame)
 		widthsum[r] = 0
 		for c=1,perrow[r] do
 			k = k + 1
-			if( k <= imagecount ) then
+			if k <= imagecount then
 				local i = imagenumbers[k]
-				if( isnotempty(totalwidth) ) then
+				if isnotempty(totalwidth) then
 					widths[k], heights[k] = getdimensions(pargs['image' .. i], pargs['width' .. i], pargs['height' .. i])
 				else
 					widths[k] = getWidth(width, pargs['width' .. i])
@@ -154,10 +168,10 @@ local function renderMultipleImages(frame)
 	if imagegap < 0 then imagegap = 0 end
 
 	-- if total_width has been specified, rescale the image widths
-	if( isnotempty(totalwidth) ) then
+	if isnotempty(totalwidth) then
 		totalwidth = tonumber(totalwidth)
 		widthmax = 0
-		local k = 0
+		k = 0
 		for r=1,rowcount do
 			local koffset = k
 			local tw = totalwidth - (3 + imagegap) * (perrow[r] - 1) - 12
@@ -165,8 +179,7 @@ local function renderMultipleImages(frame)
 			local arsum = 0
 			for j=1,perrow[r] do
 				k = k + 1
-				if( k<= imagecount ) then
-					local i = imagenumbers[k]
+				if k<= imagecount then
 					local h = heights[k] or 0
 					if (h > 0) then
 						ar[j] = widths[k]/h
@@ -182,8 +195,7 @@ local function renderMultipleImages(frame)
 			k = koffset
 			for j=1,perrow[r] do
 				k = k + 1
-				if( k<= imagecount ) then
-					local i = imagenumbers[k]
+				if k<= imagecount then
 					widths[k] = math.floor(ar[j]*ht + 0.5)
 					ws = ws + widths[k]
 					if heights[k] then
@@ -197,63 +209,75 @@ local function renderMultipleImages(frame)
 	end
 
 	-- start building the array of images, if there are images
-	if( imagecount > 0 ) then
+	if imagecount > 0 then
 		-- compute width of outer div
 		local bodywidth = 0
 		for r=1,rowcount do
-			if( widthmax == widthsum[r] ) then
+			if widthmax == widthsum[r] then
 				bodywidth = widthmax + (3 + imagegap) * (perrow[r] - 1) + 12
 			end
 		end
 		-- The body has a min-width of 100, which needs to be taken into account on specific widths
 		bodywidth = math.max( 100, bodywidth - 8);
 
-		local bg = pargs['background color'] ~= '' and pargs['background color'] or nil
+		local tc = pargs['color'] or pargs['text_color'] or ''
+		local bg = pargs['bg_color'] or pargs['background_color'] or pargs['background color'] or ''
 		-- create the array of images
 		local root = mw.html.create('div')
 		root:addClass('thumb')
 		root:addClass('tmulti')
 		-- root:addClass('tmulti-sandbox')
-		root:addClass(thumbclass[align] or 'tright')
+		root:addClass(thumbclass[align] or 'floatright')
 
-		if( align == 'center' or align == 'centre' ) then
+		if align == 'center' or align == 'centre' then
 			root:addClass('center')
 		end
-		if( bg ~= '' ) then
-			root:css('background-color', bg)
+		if tc ~= '' then
+			root:css('color', tc)
 		end
 
 		local div = root:tag('div')
-		div:addClass('thumbinner')
+		div:addClass((border ~= 'infobox') and 'thumbinner multiimageinner' or 'multiimageinner')
 		div:css('width', tostring(bodywidth) .. 'px')
 			:css('max-width', tostring(bodywidth) .. 'px')
-			:css('background-color', bg)
-		if( border == 'infobox' or border == 'none') then
+		if bg ~= '' then
+			div:css('background-color', bg)
+		end
+		if tc ~= '' then
+			div:css('color', tc)
+		end
+		if border == 'infobox' or border == 'none' then
 			div:css('border', 'none')
 		end
 		-- add the header
-		if( isnotempty(header) ) then
+		if isnotempty(header) then
 			div:tag('div')
 				:addClass('trow')
 				:tag('div')
 					:addClass('theader')
-					:css('text-align', pargs['header_align'] or 'center')
-					:css('background-color', pargs['header_background'] ~= '' and pargs['header_background'] or nil)
+					:css('text-align', pargs['header_align'])
+					:css('background-color', 
+						(pargs['header_background'] ~= '') and pargs['header_background'] or nil)
 					:wikitext(header)
 		end
 		-- loop through the images
-		local k = 0
+		k = 0
 		for r=1,rowcount do
 			local rowdiv = div:tag('div'):addClass('trow');
 			for j=1,perrow[r] do
 				k = k + 1
-				if( k <= imagecount ) then
+				if k <= imagecount then
 					local imagediv = rowdiv:tag('div')
-					imagediv
-						:addClass('tsingle')
-						:css('background-color', bg)
-					if ((imagegap > 1) and (j < perrow[r])) then
-						imagediv:css('margin-right', tostring(imagegap) .. 'px')
+					imagediv:addClass('tsingle')
+					if bg ~= '' then
+						imagediv:css('background-color', bg);
+					end
+					if imagegap > 1 and k < imagecount then
+						if dir == 'vertical' then
+							imagediv:css('margin-bottom', tostring(imagegap) .. 'px')	
+						elseif j < perrow[r] then
+							imagediv:css('margin-right', tostring(imagegap) .. 'px')
+						end
 					end
 					local i = imagenumbers[k]
 					local img = pargs['image' .. i]
@@ -262,20 +286,23 @@ local function renderMultipleImages(frame)
 						:css('max-width', tostring(2 + w) .. 'px')
 					imagediv:wikitext(renderImageCell(img, w, heights[k],
 						pargs['link' .. i], pargs['alt' .. i],
-						pargs['thumbtime' .. i], pargs['caption' .. i], capalign, imgstyle))
+						pargs['thumbtime' .. i], pargs['caption' .. i], pargs['class' .. i], capalign, imgstyle, border, tc))
 				end
 			end
 		end
 		-- add the footer
-		if( isnotempty(footer) ) then
-			local falign = string.lower(pargs['footer_align'] or args['footer_align'] or 'left')
+		if isnotempty(footer) then
+			local falign = string.lower(pargs['footer_align'] or args['footer_align'] or '')
 			falign = (falign == 'centre') and 'center' or falign
 			div:tag('div')
 				:addClass('trow')
+				:css('display', (falign ~= '') and 'flow-root' or 'flex')
 				:tag('div')
-					:addClass('thumbcaption' .. (falign == 'center' and '-center' or ''))
-					:css('text-align', (falign ~= 'left') and falign or nil)
-					:css('background-color', pargs['footer_background'] ~= '' and args['footer_background'] or nil)
+					:addClass((border ~= 'infobox') and 'thumbcaption' or nil)
+					:css('text-align', (falign ~= '') and falign or nil)
+					:css('background-color', 
+						(pargs['footer_background'] ~= '') and pargs['footer_background'] or nil)
+                    :css('color', (tc ~= '') and tc or nil)
 					:wikitext(footer)
 		end
 		return tostring(root)
@@ -286,11 +313,41 @@ end
 function p.render( frame )
 	autoscaledimages = false
 	nonautoscaledimages = false
+	numberofimages = 0
 
+	local check = require('Module:Check for unknown parameters')._check
+	local tracking = check({
+		['unknown'] = frame:expandTemplate{
+			title = 'main other',
+			args = {'[[Category:Pages using multiple image with unknown parameters|_VALUE_ ]]'}
+		},
+		['preview'] = 'Page using [[Template:Multiple image]] with unknown parameter "_VALUE_"',
+		['ignoreblank'] = 'y',
+		regexp1 = 'image%d+',
+		regexp2 = 'width%d+',
+		regexp3 = 'height%d+',
+		regexp4 = 'class%d+',
+		regexp5 = 'alt%d+',
+		regexp6 = 'link%d+',
+		regexp7 = 'thumbtime%d+',
+		regexp8 = 'caption%d+',
+		'align', 'direction', 'header_background', 
+		'header_align', 'header', 'width', 'total_width', 
+		'caption_align', 'footer_background', 'footer_align', 'footer', 
+		'perrow', 'title', 'image_gap', 'border', 'image_style',
+		'color', 'text_color',
+		'bg_color', 'background_color', 'background color'
+	}, frame:getParent().args)
+	
 	return frame:extensionTag {name = 'templatestyles', args = {src = 'Multiple image/styles.css', wrapper = ".tmulti"}}
 		.. renderMultipleImages( frame )
-		.. (autoscaledimages and '[[Категория:Страници, използващи Шаблон:Няколко картинки с автоматично оразмерени изображения]]' or '')
-		.. (nonautoscaledimages and '[[Категория:Страници, използващи Шаблон:Няколко картинки с ръчно оразмерени изображения]]' or '')
+		.. (autoscaledimages and '[[Category:Pages using multiple image with auto scaled images]]' or '')
+		.. (nonautoscaledimages and '[[Category:Pages using multiple image with manual scaled images]]' or '')
+		.. ( (numberofimages == 0) and '[[Category:Pages using multiple image with zero images]]' or '')
+		.. ( (numberofimages == 1) and '[[Category:Pages using multiple image with one image]]' or '' )
+		.. tracking
 end
+
+p[''] = function( frame ) return p.render( frame:newChild{title = frame:getTitle()} ) end
 
 return p
